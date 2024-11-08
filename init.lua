a61f4c976f4dbc6215f2ce207fff31570b741576
ROllerozxa <rollerozxa@voxelmanip.se>
2024-11-08 21:54:16 +0100

    `minetest` -> `core`

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 
 function register_road_slab(itemstring)
-	local nodedef = table.copy(minetest.registered_nodes["simple_roads:"..itemstring])
+	local nodedef = table.copy(core.registered_nodes["simple_roads:"..itemstring])
 
 	nodedef.description = nodedef.description .. " Slab"
 
@@ -11,16 +11,16 @@ function register_road_slab(itemstring)
 		fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
 	}
 
-	minetest.register_node("simple_roads:"..itemstring.."_slab", nodedef)
+	core.register_node("simple_roads:"..itemstring.."_slab", nodedef)
 end
 
-minetest.register_node("simple_roads:road", {
+core.register_node("simple_roads:road", {
 	description = "Road",
 	tiles = {"road.png"},
 	groups = {cracky = 3}
 })
 
-minetest.register_node("simple_roads:road_middleline_broken", {
+core.register_node("simple_roads:road_middleline_broken", {
 	description = "Road Middleline Broken",
 	tiles = {
 		"road_middleline_broken.png",
@@ -30,14 +30,14 @@ minetest.register_node("simple_roads:road_middleline_broken", {
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_middleline_broken_turn", {
+core.register_node("simple_roads:road_middleline_broken_turn", {
 	description = "Road Middleline Broken Turn",
 	tiles = {"road_middleline_broken_turn.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_middleline_single", {
+core.register_node("simple_roads:road_middleline_single", {
 	description = "Road Middleline Single",
 	tiles = {
 		"road_middleline_single.png",
@@ -47,14 +47,14 @@ minetest.register_node("simple_roads:road_middleline_single", {
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_middleline_single_turn", {
+core.register_node("simple_roads:road_middleline_single_turn", {
 	description = "Road Middleline Single Turn",
 	tiles = {"road_middleline_single_turn.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side", {
+core.register_node("simple_roads:road_side", {
 	description = "Road Side",
 	tiles = {
 		"road_side.png",
@@ -65,42 +65,42 @@ minetest.register_node("simple_roads:road_side", {
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_broken", {
+core.register_node("simple_roads:road_side_broken", {
 	description = "Road Side Broken",
 	tiles = {"road_side_broken.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_broken_corner", {
+core.register_node("simple_roads:road_side_broken_corner", {
 	description = "Road Side Broken Corner",
 	tiles = {"road_side_broken_corner.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_corner", {
+core.register_node("simple_roads:road_side_corner", {
 	description = "Road Side Corner",
 	tiles = {"road_side_corner.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_corner_2", {
+core.register_node("simple_roads:road_side_corner_2", {
 	description = "Road Side Corner 2",
 	tiles = {"road_side_corner_2.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_corner_parking", {
+core.register_node("simple_roads:road_side_corner_parking", {
 	description = "Road Side Corner (Parking)",
 	tiles = {"road_side_corner_parking.png", "road.png"},
 	groups = {cracky = 3},
 	paramtype2 = "facedir"
 })
 
-minetest.register_node("simple_roads:road_side_corner_parking_flipped", {
+core.register_node("simple_roads:road_side_corner_parking_flipped", {
 	description = "Road Side Corner (Parking) (Flipped)",
 	tiles = {"road_side_corner_parking.png^[transformFY", "road.png"},
 	groups = {cracky = 3},
